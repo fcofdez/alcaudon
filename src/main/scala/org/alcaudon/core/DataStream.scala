04a039f683829ff521d39d8022eebf90f4fbeb1d
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2017-02-11 01:04:59 +0100

    Creating first transformations from the dataflow.

--- src/main/scala/org/alcaudon/core/DataStream.scala
+++ src/main/scala/org/alcaudon/core/DataStream.scala
@@ -3,8 +3,20 @@ package alcaudon.core
 trait DataStream[T] {
   type Type = T
   val streamingContext: StreamingContext
+  val streamTransformation: StreamTransformation[T]
+
+  // def filter[T](fn: (T => Boolean)): DataStream[T] = {
+  //   OneInputTransformation
+  // }
+
+  def transform[O](opName: String, operator: OneInputStreamOperator[T, O]): String = {
+    val transformation = OneInputTransformation("id", opName, streamTransformation, operator)
+    streamingContext.addOperation(transformation)
+    "1"
+  }
 }
 
+case class OneSourceDataStream[T](streamingContext: StreamingContext,
+                                  streamTransformation: StreamTransformation[T]) extends DataStream[T]
 
-case class OneSourceDataStream[T](streamingContext: StreamingContext) extends DataStream[T] {
-}
+trait SourceDataStream[T] extends DataStream[T]
