9937fdeffc66dd6f9ef1d2d4a643cd7aee5a26f0
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2017-02-11 01:05:20 +0100

    Adding StreamOperator trait that encodes an operation over a StreamRecord.

---
+++ src/main/scala/org/alcaudon/core/StreamOperator.scala
@@ -0,0 +1,10 @@
+package alcaudon.core
+
+// StreamOperator encodes an operation over an StreamRecord that will
+// produce a result of type Out.
+trait StreamOperator[Out] {
+}
+
+trait OneInputStreamOperator[I, O] extends StreamOperator[I] {
+  def processStreamRecord[I](record: StreamRecord[I]): Unit
+}
