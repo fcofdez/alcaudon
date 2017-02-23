456c3630707070c9606f1c6ba77c997e5114d536
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2017-02-23 23:52:09 +0100

    Adding collector to handle data pass between operators.

---
+++ src/main/scala/org/alcaudon/core/Collector.scala
@@ -0,0 +1,14 @@
+package alcaudon.core
+
+import scala.collection.mutable.ArrayBuffer
+
+
+// This manages data transfer between operators.
+case class Collector[T]() {
+  var records = ArrayBuffer[StreamRecord[T]]()
+
+  def collect(src: T): Unit = {
+    // TODO handle timestamp
+    records += StreamRecord(src, 0L)
+  }
+}
--- src/main/scala/org/alcaudon/core/StreamOperator.scala
+++ src/main/scala/org/alcaudon/core/StreamOperator.scala
@@ -11,6 +11,12 @@ trait StreamOperator[Out] {
   var output: Output[Out] = Output()
 }
 
+case class StreamFlatMap[T, O](fn: (T, Collector[O]) => Unit) extends OneInputStreamOperator[T, O] {
+  val collector = Collector[O]()
+  def processStreamRecord(record: StreamRecord[T]): Unit =
+    fn(record.value, collector)
+}
+
 case class StreamMap[T, O](fn: T => O) extends OneInputStreamOperator[T, O] {
   def processStreamRecord(record: StreamRecord[T]): Unit =
     output = output.collect(StreamRecord(fn(record.value), record.timeStamp))
