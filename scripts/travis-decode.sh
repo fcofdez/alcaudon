f701b5a7f762d192d2099ee30c8345b9f68173ad
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2017-08-15 17:45:38 +0200

    Fixing default digest for openssl.

--- scripts/travis-decode.sh
+++ scripts/travis-decode.sh
@@ -4,5 +4,5 @@ set -ev
 
 [ ! ${TRAVIS_SECURE_ENV_VARS} ] && exit -1;
 
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in secring.gpg.enc -out secring.gpg -d
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in pubring.gpg.enc -out pubring.gpg -d
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md sha256 -in secring.gpg.enc -out secring.gpg -d
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md sha256 -in pubring.gpg.enc -out pubring.gpg -d
