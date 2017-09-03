2ed65a9b252a6de496e24200b89245615fbe5e0d
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2017-09-03 22:27:19 +0200

    Refactoring pending deployer.

--- src/main/scala/org/alcaudon/clustering/CoordinatorDataflowDeployer.scala
+++ src/main/scala/org/alcaudon/clustering/CoordinatorDataflowDeployer.scala
@@ -39,6 +39,13 @@ class CoordinatorDataflowDeployer(
       context.setReceiveTimeout(2.minutes)
   }
 
+  def checkPending(pendingCount: Int): Unit = {
+    if (pendingCount == 1 ) {
+      context.parent ! DataflowDeployed(dataflowId)
+      requester ! DataflowPipelineCreated
+    }
+  }
+
   def waitingForResponses(requester: ActorRef, pendingCount: Int): Receive = {
     case NonAvailableSlots(id) =>
       log.error(
@@ -49,32 +56,19 @@ class CoordinatorDataflowDeployer(
       context.parent ! DataflowDeploymentFailed(dataflowId)
     case ComputationDeployed(_) =>
       log.info("Computation deployed {}", pendingCount - 1)
-      if (pendingCount == 0 ) {
-        context.parent ! DataflowDeployed(dataflowId)
-        requester ! DataflowPipelineCreated
-      }
+      checkPending(pendingCount)
       context.become(waitingForResponses(requester, pendingCount - 1))
     case StreamDeployed(_) =>
       log.info("Stream deployed {}", pendingCount - 1)
-      if (pendingCount == 0 ) {
-        context.parent ! DataflowDeployed(dataflowId)
-        requester ! DataflowPipelineCreated
-      }
+      checkPending(pendingCount)
       context.become(waitingForResponses(requester, pendingCount - 1))
     case SourceDeployed(_) =>
       log.info("Source deployed {}", pendingCount - 1)
-
-      if (pendingCount == 0 ) {
-        context.parent ! DataflowDeployed(dataflowId)
-        requester ! DataflowPipelineCreated
-      }
+      checkPending(pendingCount)
       context.become(waitingForResponses(requester, pendingCount - 1))
     case SinkDeployed(_) =>
       log.info("Sink deployed {}", pendingCount - 1)
-      if (pendingCount == 0 ) {
-        context.parent ! DataflowDeployed(dataflowId)
-        requester ! DataflowPipelineCreated
-      }
+      checkPending(pendingCount)
       context.become(waitingForResponses(requester, pendingCount - 1))
     case ReceiveTimeout if pendingCount == 0 =>
       log.info("Deployment for dataflow {} finished succesfully", dataflowId)
